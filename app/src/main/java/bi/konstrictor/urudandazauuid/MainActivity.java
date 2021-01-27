adc32a6e6b587927a0a2bfb1489cfa995742664d
konstrictor <jonkur96@gmail.com>
2021-01-27 13:42:22 +0200

    done with the app

--- app/src/main/java/bi/konstrictor/urudandazauuid/MainActivity.java
+++ app/src/main/java/bi/konstrictor/urudandazauuid/MainActivity.java
@@ -1,5 +1,7 @@
 package bi.konstrictor.urudandazauuid;
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.provider.Settings;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -22,10 +25,19 @@ public class MainActivity extends Activity {
         android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
         txt_uuid.setText(android_id);
     }
-    public void send(View view) {
-//        Uri sms_uri = Uri.parse("smsto:+25775960696");
-//        Intent sms_intent = new Intent(Intent.ACTION_SENDTO, sms_uri);
-//        sms_intent.putExtra("sms_body", android_id);
-//        startActivity(sms_intent);
+    public void wtspp(View view) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("", android_id);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this,"l' UUID a été copié", Toast.LENGTH_LONG).show();
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://wa.me/+25775960696"));
+        startActivity(i);
+    }
+    public void sms(View view) {
+        Uri sms_uri = Uri.parse("smsto:+25775960696");
+        Intent sms_intent = new Intent(Intent.ACTION_SENDTO, sms_uri);
+        sms_intent.putExtra("sms_body", android_id);
+        startActivity(sms_intent);
     }
 }
